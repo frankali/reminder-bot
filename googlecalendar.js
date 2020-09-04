7f70b06fd95be6e00a37fd645855300f88abf1e9
James Wong <jameswong8@gmail.com>
2020-09-04 04:40:19 -0400

    Development -> Master (#1)

--- googlecalendar.js
+++ googlecalendar.js
@@ -7,7 +7,13 @@ const axios = require('axios');
 const stdin = process.openStdin();
 const lodash = require('lodash');
 const DateTime = require('datetime-converter-nodejs');
+const chrono = require('chrono-node');
+const moment = require('moment-timezone');
 
+let auth;
+let mapNumToEventId = {};
+
+const timezone = 'America/New_York';
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
@@ -20,33 +26,22 @@ const TOKEN_PATH = 'token.json';
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), getActionFromUser);
-  //authorize(JSON.parse(content), addEvent);
-});
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {
+   const {
     client_secret,
     client_id,
     redirect_uris
-  } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
+  } = JSON.parse(content).installed;
+
+  auth = new google.auth.OAuth2(
     client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+    if (err) return getAccessToken(null);
+    auth.setCredentials(JSON.parse(token));
   });
-}
+
+});
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -54,10 +49,11 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
+function getAccessToken(callback, userArgs = []) {
+  const authUrl = auth.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
+    // approval_prompt: "force"
   });
   console.log('Authorize this app by visiting this url:', authUrl);
   const rl = readline.createInterface({
@@ -66,15 +62,17 @@ function getAccessToken(oAuth2Client, callback) {
   });
   rl.question('Enter the code from that page here: ', (code) => {
     rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
+    auth.getToken(code, (err, token) => {
       if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
+      console.log(123)
+      console.log(token);
+      auth.setCredentials(token);
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
         if (err) return console.error(err);
         console.log('Token stored to', TOKEN_PATH);
       });
-      callback(oAuth2Client);
+      if (callback)
+        callback(...userArgs);
     });
   });
 }
@@ -87,205 +85,176 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listEvents(auth) {
-  const calendar = google.calendar({
-    version: 'v3',
-    auth
-  });
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date; //convert this time to just "10PM"
-        console.log(`${start} - ${event.summary} - ${event.id} `);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
+function listEvents() {
+  //singleEvents -- boolean default false -> show al levents
+  return new Promise((resolve, reject) => {
+    let response = "";
+    const calendar = google.calendar({
+      version: 'v3',
+      auth
+    });
+    calendar.events.list({
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+      timeZone: timezone
+    }, (err, res) => {
+      if (err) reject('The API returned an error: ' + err);
+      const events = res.data.items;
+
+      if (events.length) {
+        response += "Upcoming 10 events:\n";
+        Promise.all(events.map((event,i) => {
+          return new Promise((resolve, reject) => {
+            const startTime = moment.tz(event.start.dateTime, timezone).format('L LT')
+            const endTime = moment.tz(event.end.dateTime, timezone).format('L LT')
+
+            mapNumToEventId[i+1] = event.id;
+            //convert this time to just "10PM"
+            resolve(`${i+1}: ${event.summary} -  ${startTime} - ${endTime} - ${event.id}`);
+          });
+        })).then(responses => resolve(responses.join('\n')));
+      } else {
+        resolve('No upcoming events found.');
+      }
+    });
   });
 }
 
+
+function parseEventToISO(startTime, duration) {
+  console.log(startTime, duration)
+  let parsedDate = chrono.parseDate(startTime);
+  let offset = moment.tz(timezone).utcOffset();
+  let adjusted_startTime = moment(parsedDate).subtract(offset, 'minutes').tz(timezone).toISOString();
+  let adjusted_endTime = moment(adjusted_startTime).add(duration, 'minutes').tz(timezone).toISOString();
+  console.log(adjusted_startTime + adjusted_endTime)
+  return {adjusted_startTime, adjusted_endTime};
+}
 
 /**
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function addEvent(auth) {
-  var event = {
-    'summary': 'Project-TBD: Meeting', //variable
-    'location': 'Discord: Project-TBD Channel',
-    'description': 'A Project TBD Meeting',
-    'start': {
-      'dateTime': '2020-09-05T02:00:00-05:00',
-      'timeZone': 'America/New_York',
-    },
-    'end': {
-      'dateTime': '2020-09-05T17:00:00-04:00',
-      'timeZone': 'America/New_York',
-    },
-    // 'recurrence': [
-    //   'RRULE:FREQ=WEEKLY;INTERVAL=1'
-    // ],
-    'attendees': [{
-        'email': 'test@gmail.com'
+function addEvent(name, startTime, duration=60, recurrence=false) {
+  //2020-09-05T02:00:00-05:00
+  //2020-09-05T17:00:00-04:00
+  return new Promise((resolve, reject) => {
+    let {adjusted_startTime, adjusted_endTime} = parseEventToISO(startTime, duration);
+    console.log(adjusted_startTime);
+    console.log(adjusted_endTime);
+    var event = {
+      'summary': name, //variable
+      'location': 'Discord: Project-TBD Channel',
+      'description': name,
+      'start': {
+        'dateTime': adjusted_startTime,
+        'timeZone': timezone,
       },
-      {
-        'email': 'sbrin@example.com'
+      'end': {
+        'dateTime': adjusted_endTime,
+        'timeZone': timezone,
       },
-    ],
-  };
-  const calendar = google.calendar({
-    version: 'v3',
-    auth
-  });
-  calendar.events.insert({
-    auth: auth,
-    calendarId: 'primary',
-    resource: event,
-  }, function(err, event) {
-    if (err) {
-      console.log('There was an error contacting the Calendar service: ' + err);
-      return;
-    }
-    console.log('Event created: %s', event.htmlLink);
-  });
-}
+    };
+    if (recurrence)
+      event['recurrence'] = ['RRULE:FREQ=WEEKLY;INTERVAL=1'];
 
-function deleteEvent(auth, evId) {
-  const calendar = google.calendar({
-    version: 'v3',
-    auth
-  })
-  console.log(evId)
-  calendar.events.delete({
-    calendarId: 'primary',
-    eventId: evId
-  }, (err, res) => {
-    if (err) return console.log(err)
-    if (res) {
-      console.log('Event deleted!')
-    }
-  })
-}
-
-function updateEvent(auth, evId) {
-  const calendar = google.calendar({
-    version: 'v3',
-    auth
-  })
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    console.log(evId)
-    var event;
-    events.forEach((item, i) => {
-      if (item.id === evId) {
-        event = item;
-      }
+    const calendar = google.calendar({
+      version: 'v3',
+      auth
     });
-
-    var time1 = 'Sat Sep 12 2020  15:00:00 GMT-0400 (IST)'
-    var time2 = 'Sat Sep 12 2020  17:00:00 GMT-0400 (IST)'
-    event.start.dateTime = DateTime.isoString(time1);
-    event.end.dateTime = DateTime.isoString(time2);
-  calendar.events.update({
-    auth: auth,
-    calendarId: 'primary',
-    eventId: evId,
-    resource: event,
-  }, (err, res) => {
-    if (err) return console.log(err)
-    const event = res.data
-
-    if (event) {
-      console.log('Booked event:')
-      console.log(event)
-    }
-  })
-  // console.log(DateTime.isoString(time1));
-  // console.log(DateTime.isoString(time2));
-
-
-  //     var event = lodash.map(events, function(x) {
-  //     if (x.id == "nbn00vmlp8esb0jsavidudqj68_20200912T140000") return x;
-  // });
-  // event = lodash.without(event, undefined)
-
-
-});
-
-// const calendar = google.calendar({
-//   version: 'v3',
-//   auth
-// })
-// console.log(evId)
-// calendar.events.update({
-//   calendarId: 'primary',
-//   eventId: evId,
-//   event: event
-// }, (err, res) => {
-//   if (err) return console.log(err)
-//   if (res) {
-//     console.log('Event updated!')
-//   }
-// })
+    calendar.events.insert({
+      auth: auth,
+      calendarId: 'primary',
+      resource: event,
+    }, function(err, event) {
+      console.log("in insert")
+      if (err) {
+        reject('There was an error contacting the Calendar service: ' + err);
+      }
+      response = 'Event created!';
+      resolve(response);
+    });
+  });
 }
 
-function getActionFromUser(auth) {
-  const numberOfListedEvents = 100
-  console.log(`10 - Lists your ${numberOfListedEvents} first Google calendar events`)
-  // console.log(`11 - Lists your ${numberOfListedEvents} first Google calendar events from Today`)
-  console.log('20 - Inserts new event for tomorrow')
-  console.log('30 - Delete an event by ID')
-  console.log('40 - Update an event by ID')
-  console.log('\n0 - Exit')
-  console.log('\n')
-  console.log('Choose an action:')
-
-  stdin.addListener("data", function(d) {
-    switch (Number(d)) {
-      case 10:
-        listEvents(auth)
-        break
-        // case 11:
-        //   listEvents(auth, numberOfListedEvents)
-        //   break
-      case 20:
-        addEvent(auth)
-        break
-      case 30:
-        // console.log('Enter event id')
-        // stdin.addListener("data", function(d) {
-        //   console.log(d. + "ok")
-        //       convertedId = (String(d))
-      //  deleteEvent(auth, "nbn00vmlp8esb0jsavidudqj68_20200905T140000Z")  //delete single
-      deleteEvent(auth, "nbn00vmlp8esb0jsavidudqj68") //delete recurring
-        // });
-        break
-      case 40:
-        updateEvent(auth, "ip4s5heeb3co0ipj9q97sttas0")
-        break
-      case 0:
-        process.exit()
-        break
-    }
+function deleteEvent(id) {
+  //recurringEventId - field of eventsId
+   return new Promise((resolve, reject) => {
+    const calendar = google.calendar({
+      version: 'v3',
+      auth
+    })
+    calendar.events.delete({
+      calendarId: 'primary',
+      eventId: mapNumToEventId[id]
+    }, (err, res) => {
+      if (err) reject(err)
+      if (res) {
+        response = "Event deleted";
+        resolve(response)
+      }
+    })
   });
+}
+
+function updateEvent(evId, startTime, duration=60) {
+   return new Promise((resolve, reject) => {
+    const calendar = google.calendar({
+      version: 'v3',
+      auth
+    })
+
+    calendar.events.get({
+      calendarId: 'primary',
+      eventId: evId,
+    }, (err, res) => {
+      if (err) return console.log(err)
+      const event = res.data;
+      let {adjusted_startTime, adjusted_endTime} = parseEventToISO(startTime, duration);
+      event.start.dateTime = adjusted_startTime;
+      event.end.dateTime = adjusted_endTime;
+
+      calendar.events.update({
+        auth: auth,
+        calendarId: 'primary',
+        eventId: evId,
+        resource: event,
+      }, (err, res) => {
+        if (err) reject(err)
+        const event = res.data
+
+        if (event) {
+          let response = ('Event has been updated!')
+          resolve(response);
+        }
+      });
+    });
+  });
+}
+
+function calendarAPIController(userCmd, userArgs) {
+  switch(userCmd) {
+    case 'list':
+    console.log('list')
+      return listEvents()
+      break;
+    case 'add':
+      return addEvent(...userArgs) //id, start time, endtime
+      break;
+    case 'delete':
+      return deleteEvent(...userArgs) //id, start time, endtime
+      break;
+    case 'update':
+      return updateEvent(...userArgs) //id, start time, endtime
+      break;
+
+  }
 }
 
 //check for events at every day...
 // when new event is added, re-check events for reminders.. -> send out if happening that delayed
 // same for editEvent
+module.exports = calendarAPIController;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require the discord.js module
+const calendarAPIController = require('./googlecalendar.js')
 const Discord = require('discord.js');
-//const gCalendarAPI = require('./googlecalendar.js')
 
 const {
   prefix,
@@ -19,35 +19,52 @@ client.once('ready', () => {
 
 //First, make sure to close the process in your console.
 client.on('message', message => {
-  console.log(message);
+  //console.log(message);
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).trim().split(' ');
-  console.log("ARGS" + args)
+  //console.log("ARGS" + args)
   const command = args.shift();
-  console.log("CMD" + command)
+  let rejoinedArgs = args.join(' ').match(/"([^"]+)"/g) || [];
+  console.log("CMD entered: " + command)
   if (`${prefix}${command}` === `${prefix}calendarBot`) {
-    message.channel.send('CalendarBot v1.0 for Project-Tbd!);
+    message.channel.send('CalendarBot v1.0 for Project-Tbd!');
     message.channel.send('This bot is a simple way to create reminders for discord meetings');
     message.channel.send('Use !cb-help for more information!');
   } else if (`${prefix}${command}` == `${prefix}cb-help`) {
     message.channel.send('To schedule an event use the following format: ');
-    message.channel.send('!create-event-reminder [event-name] [start time] [end time] [recurring-type]');
-    message.channel.send(`[event-name]: Test-Meeting`);
-    message.channel.send(`[start time]: Sat Sep 12 2020  15:00:00`);
-    message.channel.send(`[end time]: Sat Sep 12 2020  17:00:00`);
-    message.channel.send(`[recurring type]: weekly, daily, monthly, null`
-  } else if (`${prefix}${command}` === `${prefix}commands`) {
-    if (!args.length) {
-      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-    }
-    message.channel.send('');
-		//message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    message.channel.send('!update-event [event-id] [start time] [end time]');
+    // message.channel.send(`[event-name]: Test-Meeting`);
+    // message.channel.send(`[start time]: Sat Sep 12 2020  15:00:00`);
+    // message.channel.send(`[end time]: Sat Sep 12 2020  17:00:00`);
+    // message.channel.send(`[recurring type]: weekly, daily, monthly, null`
+  } else { // (`${prefix}${command}` === `${prefix}list-event`) {
+		calendarAPIController(command, rejoinedArgs.map(arg => arg.slice(1,-1)))
+      .then(response => {
+        message.channel.send(response);
+      }, failure => {
+        message.channel.send(failure);
+      });
   }
 })
 
 // login to Discord with your app's token
 client.login(token);
 
+// function shittyParser(argS) {
+//  let seenOpenQuote = false;
+//  let args = [];
+//  let arg = "";
+//  for(i = 0 ; i < argS.length; i++) {
+//    if (argS == '"') {
+//      if(seenOpenQuote) {
+//         args.push(arg);
+//      }
+//      seenOpenQuote = true;
+//    } else {
+//      arg += argS[i];
+//    }
+//  }
+// }
 //single row
 // recurring : date : time :(exception: 9/5...)
 // single: 9/5 :..
